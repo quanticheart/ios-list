6fc182a5810e75d31e29425252cd885ae600c2d4
jonnyz <dev.jonny255d@gmail.com>
2022-03-09 19:27:09 -0300

    Initial Commit

---
+++ MoviesSlib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesSlib
+//
+//  Created by Jonatas Alves santos on 09/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
